2e001b7d39528ef74e68f6e93cd6684885ecadef
Christian Hamamdjian <christian.hamamdjian@welho.com>
2021-03-26 16:52:51 +0200

    layout with cms functionalities

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,8 +1,15 @@
 <?php
-
-define("DB_SERVER", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "root");
-define("DB_NAME", "opepi");
-
+$local = in_array($_SERVER['REMOTE_ADDR'],array('127.0.0.1','REMOTE_ADDR' => '::1'));
+if (!$local) {	
+    define("DB_SERVER", "localhost:49404");
+    define("DB_USER", "azure");
+    define("DB_PASS", "6#vWHD_$");
+    define("DB_NAME", "opepi");
+  }
+else {
+    define("DB_SERVER", "localhost");
+    define("DB_USER", "root");
+    define("DB_PASS", "root");
+    define("DB_NAME", "opepi");
+  }
 ?>
